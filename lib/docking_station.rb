08c3b8e85339d2398019fd4232a02b861e115d82
Else Meijerink <elsemeijerink@MA305.lan>
2021-06-30 19:23:50 +0100

    Implemented bike docking functionality and tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 require './lib/bike.rb'
 
 class DockingStation
+    attr_reader :docked_bike
+
     def release_bike
         Bike.new
+    end
+
+    def dock_bike(bike)
+        @docked_bike = bike
     end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,12 +1,24 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
+    it {is_expected.to respond_to(:dock_bike)}
 
     it 'gets bike and checks if working' do
         bike = subject.release_bike
         expect(bike.class).to eq(Bike)
         expect(bike.working?).to eq(true)
+    end
+
+    it 'when docking bike add bike to dock' do
+        bike = Bike.new
+        subject.dock_bike(bike)
+        expect(subject.docked_bike).to eq(bike)
+    end
+
+    it 'given empty dock then there is no bike' do
+        expect(subject.docked_bike).to eq(nil)
     end
 end
 
