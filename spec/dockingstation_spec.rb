381d716d1f505edc51399c7b461540f3d56991a9
Else Meijerink <elsemeijerink@MA305.lan>
2021-06-30 18:54:23 +0100

    Impleemented docking station tests and functionality

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,12 @@
-require "docking_station"
+require 'docking_station'
 
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
+
+    it 'gets bike and checks if working' do
+        bike = subject.release_bike
+        expect(bike.class).to eq(Bike)
+        expect(bike.working?).to eq(true)
+    end
 end
+
